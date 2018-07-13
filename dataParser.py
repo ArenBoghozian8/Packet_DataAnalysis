6954047234c388f9b8db75a0af41923fc8266851
Aren Mark Boghozian <aren.mboghozian@gmail.com>
2018-07-12 21:02:45 -0700

    Final Version

--- dataParser.py
+++ dataParser.py
@@ -14,40 +14,39 @@ for i in range(len(experiments)):
 
 	os.mkdir('TestResults/'+experiments[i]+'/dataAnalysis/JsonInfo')
 
-	indir = 'TestResults/'+experiments[i]+'/dataAnalysis'
-	for root, dirs, filenames in os.walk(indir):
-		for f in filenames:
-			f2 = open('TestResults/'+experiments[i]+'/dataAnalysis/'+f,'r')
+	for f in os.listdir('TestResults/'+experiments[i]+'/dataAnalysis'):
+			
+		f2 = open('TestResults/'+experiments[i]+'/dataAnalysis/'+f,'r')
 
-			packetId = ''
-			timeStamp = ''
-			source = ''
-			destination = ''
-			destinationPort = ''
-			size = ''
-			fileName = f
-			isComplete = False
+		packetId = ''
+		timeStamp = ''
+		source = ''
+		destination = ''
+		destinationPort = ''
+		size = ''
+		fileName = f
+		isComplete = False
 
-			for line in f2:
-				if 'Data:' in line:
-					packetIdHex = line[10:14]
-					packetId = int(str(packetIdHex),16)
-				elif 'Arrival Time:' in line:
-					timeStamp = line[18:-1]
-				elif 'Source' in line and ('Source Port:' not in line and 'Source: Alcatel-_8d:ac:f1 (e8:e7:32:8d:ac:f1)' not in line):
-					source = line[12:-1]
-				elif 'Destination:' in line and 'Port:' not in line:
-					destination = line[17:-1]
-				elif 'Destination Port:' in line:
-					destinationPort = line[22:-1]
-				elif '[Length:' in line:
-					size = line[13:17]
-					isComplete = True
+		for line in f2:
+			if 'Data:' in line:
+				packetIdHex = line[10:14]
+				packetId = int(str(packetIdHex),16)
+			elif 'Arrival Time:' in line:
+				timeStamp = line[18:-1]
+			elif 'Source' in line and ('Source Port:' not in line and 'Source: Alcatel-_8d:ac:f1 (e8:e7:32:8d:ac:f1)' not in line):
+				source = line[12:-1]
+			elif 'Destination:' in line and 'Port:' not in line:
+				destination = line[17:-1]
+			elif 'Destination Port:' in line:
+				destinationPort = line[22:-1]
+			elif '[Length:' in line:
+				size = line[13:17]
+				isComplete = True
 
 
-				if isComplete:
-					r = {packetId:{'timeStamp':timeStamp,'Source IP':source,'Destination IP':destination,'Destination Port':destinationPort,'Size':size,'File Name':fileName}}
-					with open('TestResults/'+experiments[i]+'/dataAnalysis/JsonInfo/'+f[:-4] +'.json', 'a') as feedsjson:
-						json.dump(r, feedsjson)
-						feedsjson.write(os.linesep)
-					isComplete = False
+			if isComplete:
+				r = {packetId:{'timeStamp':timeStamp,'Source IP':source,'Destination IP':destination,'Destination Port':destinationPort,'Size':size,'File Name':fileName}}
+				with open('TestResults/'+experiments[i]+'/dataAnalysis/JsonInfo/'+f[:-4] +'.json', 'a') as feedsjson:
+					json.dump(r, feedsjson)
+					feedsjson.write(os.linesep)
+				isComplete = False
