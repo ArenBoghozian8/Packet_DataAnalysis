51853282f99caf16e2963fdb112c23b5162984a4
Aren Mark Boghozian <aren.mboghozian@gmail.com>
2018-07-13 15:24:42 -0700

    Final Version

--- dataParser.py
+++ dataParser.py
@@ -94,8 +94,8 @@ class structureData:
 
 def main():
 	experiments = ['Compression','SPQ','ShapingFinal']
-	#parse = jasonParser()
-	#parse.generateJason(experiments)
+	parse = jasonParser()
+	parse.generateJason(experiments)
 
 	struct = structureData()
 	struct.restructure(experiments)
